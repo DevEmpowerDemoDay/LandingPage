ccfd8ccfcde9f10c7ca1e32eee6443c7bb3edc0a
fatekkl <mathtml.1105@gmail.com>
2024-12-02 16:49:22 -0300

    add new page + prototype

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const btn = document.getElementById("end_button");
+const btn = document.getElementById("scroll_button")
+
 
 btn.addEventListener("click", () => {
-    alert("clicou");
-});
+    const end_page = document.querySelector(".ending_page")
+
+    end_page.scrollIntoView({behavior: "smooth"})
+})

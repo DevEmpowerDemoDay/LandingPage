445b54af8a3a5c5964b7237a0ed662054342cb3d
fatekkl <mathtml.1105@gmail.com>
2024-12-05 12:33:16 -0300

    update

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const btn = document.getElementById("scroll_button")
+const mockup = document.querySelector(".mockup_cellphone")
 
 
 btn.addEventListener("click", () => {
     const target = document.querySelector(".video_page_container")
 
     target.scrollIntoView({behavior: "smooth"})
+})
+
+
+const back_to = document.getElementById("back_to")
+
+back_to.addEventListener("click", () => {
+    mockup.src = "assets/03.png"
 })
